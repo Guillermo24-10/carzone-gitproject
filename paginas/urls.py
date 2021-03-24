1f3226ea093e9eb957fb83b1e53a1ce0ce105727
Guillermo24-10 <pgaugusto24@gmail.com>
2021-03-24 15:03:21 -0500

    creacion de pages y configuration navs

--- paginas/urls.py
+++ paginas/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('about',views.about,name='about'),
+    path('servicios',views.servicios,name='servicios'),
+    path('contacto',views.contacto,name='contacto'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'paginas/home.html')
+
+def about(request):
+    return render(request, 'paginas/about.html')
+
+def servicios(request):
+    return render(request, 'paginas/servicios.html')
+
+def contacto(request):
+    return render(request, 'paginas/contacto.html')
